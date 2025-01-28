6bf12b30e10a245ca90c45ae8acd5ded701d1e7f
manipreetham <70264118+manipreetham@users.noreply.github.com>
2025-01-28 22:40:01 +0530

    Update mcq.py

--- mcq.py
+++ mcq.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 from fpdf import FPDF  # pip install fpdf
 
 # Set your API key
-os.environ["GOOGLE_API_KEY"] = "AIzaSyBsPwZud-Ovh4tgtYNtSOpVa-WqS6yBB6Q"
+os.environ["GOOGLE_API_KEY"] = "your api key"
 # Use the correct environment variable name 'GOOGLE_API_KEY' to access the API key
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 model = genai.GenerativeModel("models/gemini-1.5-pro")
